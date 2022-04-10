a4dc4391d32f484c1b6317257c61e153ae4089c4
serifgungor <gungoronline@gmail.com>
2022-04-10 09:42:34 +0300

    SplashActivity oluşturuldu

---
+++ app/src/main/java/com/yemekapp/android/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.yemekapp.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Beklet().start();
+    }
+
+    class Beklet extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+    }
+
+}
