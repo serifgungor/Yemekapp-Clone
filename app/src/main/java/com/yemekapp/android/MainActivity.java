1eadd82329dee1be3d5fe884daafa1557e608121
serifgungor <gungoronline@gmail.com>
2022-04-10 10:49:17 +0300

    MainActivity sayfası arayüzü tasarlandı

--- app/src/main/java/com/yemekapp/android/MainActivity.java
+++ app/src/main/java/com/yemekapp/android/MainActivity.java
@@ -1,14 +1,54 @@
 package com.yemekapp.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbarMain;
+    TextView tvYemekSiparis,tvAlisveris,tvHizliMarket;
+
+    public void init(){
+        //Androidin varsayılanındaki toolbarı iptal edip, yeni bir toolbar oluşturup onu kullandık.
+        //this.getSupportActionBar().hide(); //Var olan toolbarı gizledik.
+        toolbarMain = findViewById(R.id.toolbarMain);
+        this.setSupportActionBar(toolbarMain);
+
+        tvYemekSiparis = findViewById(R.id.tvYemekSiparisi);
+        tvAlisveris = findViewById(R.id.tvAlisveris);
+        tvHizliMarket = findViewById(R.id.tvHizliMarket);
+
+        tvYemekSiparis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        tvAlisveris.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        tvHizliMarket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+
     }
 }
