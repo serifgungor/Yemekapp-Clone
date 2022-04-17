26d34f80b6c4b07492054ad1ab0c9df558ebc491
serifgungor <gungoronline@gmail.com>
2022-04-17 09:18:08 +0300

    recyclerview RestoranlariListeleActivitye bağlandı

--- app/src/main/java/com/yemekapp/android/Activity/RestoranlariListeleActivity.java
+++ app/src/main/java/com/yemekapp/android/Activity/RestoranlariListeleActivity.java
@@ -1,6 +1,8 @@
 package com.yemekapp.android.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -8,13 +10,19 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.yemekapp.android.Adapter.RV_RestoranAdapter;
+import com.yemekapp.android.Model.Restoran;
 import com.yemekapp.android.R;
+
+import java.util.ArrayList;
 
 public class RestoranlariListeleActivity extends AppCompatActivity {
 
     EditText etRestoranAra;
     ImageView ivHome;
     RecyclerView rvRestoranlariListele;
+    RecyclerView.LayoutManager layoutManager;
+    RV_RestoranAdapter adapter;
 
     public void init(){
         rvRestoranlariListele = findViewById(R.id.rvRestoranlariListele);
@@ -27,10 +35,29 @@ public class RestoranlariListeleActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public ArrayList<Restoran> getRestoranlar(){
+        ArrayList<Restoran> restoranlar = new ArrayList<>();
+
+        restoranlar.add(new Restoran(1,"Ad 1",34,"Sultangazi Uğurmumcu","Adres","https://cdn.yemeksepeti.com/adm/ramazan_new_web.jpg",0,30,20));
+        restoranlar.add(new Restoran(2,"Ad 2",34,"Sultangazi Uğurmumcu","Adres","https://cdn.yemeksepeti.com/adm/ramazan_new_web.jpg",0,30,20));
+        restoranlar.add(new Restoran(3,"Ad 3",34,"Sultangazi Uğurmumcu","Adres","https://cdn.yemeksepeti.com/adm/ramazan_new_web.jpg",0,30,20));
+
+        return restoranlar;
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restoranlari_listele);
         init();
+
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        adapter = new RV_RestoranAdapter(getRestoranlar());
+        rvRestoranlariListele.setLayoutManager(layoutManager);
+        rvRestoranlariListele.setAdapter(adapter);
+        rvRestoranlariListele.setItemAnimator(new DefaultItemAnimator());
     }
 }
